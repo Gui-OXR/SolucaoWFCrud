ca69c97f1643bb93046007fb07506551cf44d86c
LabInfo <LabInfo@TERREOA3M09>
2024-10-01 11:44:35 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoWFCrud/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace ProjetoWFCrud
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.crudToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasComFiltroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatóriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatórioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatórioComFiltroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crudToolStripMenuItem,
+            this.consultasToolStripMenuItem,
+            this.relatóriosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // crudToolStripMenuItem
+            // 
+            this.crudToolStripMenuItem.Name = "crudToolStripMenuItem";
+            this.crudToolStripMenuItem.Size = new System.Drawing.Size(66, 39);
+            this.crudToolStripMenuItem.Text = "Crud";
+            // 
+            // consultasToolStripMenuItem
+            // 
+            this.consultasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasComFiltroToolStripMenuItem});
+            this.consultasToolStripMenuItem.Name = "consultasToolStripMenuItem";
+            this.consultasToolStripMenuItem.Size = new System.Drawing.Size(105, 39);
+            this.consultasToolStripMenuItem.Text = "Consultas";
+            // 
+            // consultasComFiltroToolStripMenuItem
+            // 
+            this.consultasComFiltroToolStripMenuItem.Name = "consultasComFiltroToolStripMenuItem";
+            this.consultasComFiltroToolStripMenuItem.Size = new System.Drawing.Size(273, 34);
+            this.consultasComFiltroToolStripMenuItem.Text = "Consultas com filtro";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            this.relatóriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.relatórioToolStripMenuItem,
+            this.relatórioComFiltroToolStripMenuItem});
+            this.relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            this.relatóriosToolStripMenuItem.Size = new System.Drawing.Size(106, 39);
+            this.relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // relatórioToolStripMenuItem
+            // 
+            this.relatórioToolStripMenuItem.Name = "relatórioToolStripMenuItem";
+            this.relatórioToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.relatórioToolStripMenuItem.Text = "Relatório Agrupado";
+            // 
+            // relatórioComFiltroToolStripMenuItem
+            // 
+            this.relatórioComFiltroToolStripMenuItem.Name = "relatórioComFiltroToolStripMenuItem";
+            this.relatórioComFiltroToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.relatórioComFiltroToolStripMenuItem.Text = "Relatório com Filtro";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 422);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 28);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMenu";
+            this.Text = "Menu Principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem crudToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasComFiltroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatóriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatórioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatórioComFiltroToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ ProjetoWFCrud/Formularios/frmCrud.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace ProjetoWFCrud.Formularios
+{
+    partial class frmCrud
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCrud));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tbsNovo = new System.Windows.Forms.ToolStripButton();
+            this.tbsSalvar = new System.Windows.Forms.ToolStripButton();
+            this.tbsCancelar = new System.Windows.Forms.ToolStripButton();
+            this.tbsExcluir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tbsBuscaPorId = new System.Windows.Forms.ToolStripLabel();
+            this.tstId = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMedida = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.mskValidade = new System.Windows.Forms.MaskedTextBox();
+            this.mskPreco = new System.Windows.Forms.MaskedTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbsNovo,
+            this.tbsSalvar,
+            this.tbsCancelar,
+            this.tbsExcluir,
+            this.toolStripSeparator1,
+            this.tbsBuscaPorId,
+            this.tstId,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(960, 46);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tbsNovo
+            // 
+            this.tbsNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbsNovo.Image = ((System.Drawing.Image)(resources.GetObject("tbsNovo.Image")));
+            this.tbsNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbsNovo.Name = "tbsNovo";
+            this.tbsNovo.Size = new System.Drawing.Size(34, 41);
+            this.tbsNovo.Text = "Novo";
+            // 
+            // tbsSalvar
+            // 
+            this.tbsSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbsSalvar.Image = ((System.Drawing.Image)(resources.GetObject("tbsSalvar.Image")));
+            this.tbsSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbsSalvar.Name = "tbsSalvar";
+            this.tbsSalvar.Size = new System.Drawing.Size(34, 41);
+            this.tbsSalvar.Text = "Salvar";
+            // 
+            // tbsCancelar
+            // 
+            this.tbsCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbsCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tbsCancelar.Image")));
+            this.tbsCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbsCancelar.Name = "tbsCancelar";
+            this.tbsCancelar.Size = new System.Drawing.Size(34, 41);
+            this.tbsCancelar.Text = "Cancelar";
+            // 
+            // tbsExcluir
+            // 
+            this.tbsExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbsExcluir.Image = ((System.Drawing.Image)(resources.GetObject("tbsExcluir.Image")));
+            this.tbsExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbsExcluir.Name = "tbsExcluir";
+            this.tbsExcluir.Size = new System.Drawing.Size(34, 41);
+            this.tbsExcluir.Text = "Excluir";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 46);
+            // 
+            // tbsBuscaPorId
+            // 
+            this.tbsBuscaPorId.Name = "tbsBuscaPorId";
+            this.tbsBuscaPorId.Size = new System.Drawing.Size(111, 41);
+            this.tbsBuscaPorId.Text = "Busca por Id";
+            // 
+            // tstId
+            // 
+            this.tstId.Name = "tstId";
+            this.tstId.Size = new System.Drawing.Size(121, 46);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(34, 41);
+            this.toolStripButton5.Text = "tsbBuscar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(16, 78);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(55, 26);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNome.Location = new System.Drawing.Point(94, 78);
+            this.txtNome.MaxLength = 50;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(207, 26);
+            this.txtNome.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtMedida
+            // 
+            this.txtMedida.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtMedida.Location = new System.Drawing.Point(326, 78);
+            this.txtMedida.MaxLength = 50;
+            this.txtMedida.Name = "txtMedida";
+            this.txtMedida.Size = new System.Drawing.Size(207, 26);
+            this.txtMedida.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(322, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Medida";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(564, 78);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(166, 26);
+            this.txtQuantidade.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(560, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Quantidade";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(16, 163);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(714, 26);
+            this.txtDescricao.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Descrição";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Validade";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(322, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Preço";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // mskValidade
+            // 
+            this.mskValidade.Location = new System.Drawing.Point(16, 265);
+            this.mskValidade.Mask = "00/00/0000";
+            this.mskValidade.Name = "mskValidade";
+            this.mskValidade.Size = new System.Drawing.Size(285, 26);
+            this.mskValidade.TabIndex = 15;
+            this.mskValidade.ValidatingType = typeof(System.DateTime);
+            // 
+            // mskPreco
+            // 
+            this.mskPreco.Location = new System.Drawing.Point(326, 265);
+            this.mskPreco.Mask = "$";
+            this.mskPreco.Name = "mskPreco";
+            this.mskPreco.Size = new System.Drawing.Size(285, 26);
+            this.mskPreco.TabIndex = 16;
+            // 
+            // frmCrud
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mskPreco);
+            this.Controls.Add(this.mskValidade);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMedida);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmCrud";
+            this.Text = "frmCrud";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tbsNovo;
+        private System.Windows.Forms.ToolStripButton tbsSalvar;
+        private System.Windows.Forms.ToolStripButton tbsCancelar;
+        private System.Windows.Forms.ToolStripButton tbsExcluir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel tbsBuscaPorId;
+        private System.Windows.Forms.ToolStripComboBox tstId;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMedida;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox mskValidade;
+        private System.Windows.Forms.MaskedTextBox mskPreco;
+    }
+}
